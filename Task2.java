463541c77dadd57d5e6ae9104ce325588011aa23
BritinaPearlsidaB <161219289+BritinaPearlsidaB@users.noreply.github.com>
2024-03-11 20:05:49 +0530

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Task2 {
+    //STUDENT GRADE CALCULATOR
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of subjects: ");
